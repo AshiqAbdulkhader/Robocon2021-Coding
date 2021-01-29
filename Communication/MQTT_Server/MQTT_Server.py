b8944cbdf87c379321c04d09f43102311796d233
AshiqAbdulkhader <ashiqabdulkhader@gmail.com>
2021-01-29 23:32:35 +0530

    Update MQTT_Server.py

--- Communication/MQTT_Server/MQTT_Server.py
+++ Communication/MQTT_Server/MQTT_Server.py
@@ -5,8 +5,9 @@ mqttc.connect("localhost",1883,60)                       # connect(host, port=18
 mqttc.loop_start()
 
 while(1):
-	c=int(input("Enter a state(0/1) : "))
-	if(c==1 or c==0):
-		mqttc.publish("esp8266/4","1")          #publish(topic, payload=None, qos=0, retain=False)
-	
+        c=int(input("Enter a state(0/1) : "))
+        if(c==1):
+                mqttc.publish("esp8266/4","1")          #publish(topic, payload=None, qos=0, retain=False)
+        elif(c==0):
+                mqttc.publish("esp8266/4","0")
 	
